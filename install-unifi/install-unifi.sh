fd8150672a5cedb16a755c98b24c6c199c5b2f00
Jorge Frade <jamfrade@users.noreply.github.com>
2023-11-26 11:33:01 +0000

    Update install-unifi.sh

--- install-unifi/install-unifi.sh
+++ install-unifi/install-unifi.sh
@@ -4,11 +4,11 @@
 # Installs the Uni-Fi controller software on a FreeBSD machine (presumably running pfSense).
 
 # The latest version of UniFi:
-UNIFI_SOFTWARE_URL="https://dl.ui.com/unifi/7.2.97/UniFi.unix.zip"
+UNIFI_SOFTWARE_URL="https://dl.ui.com/unifi/8.0.7/UniFi.unix.zip"
 
 
 # The rc script associated with this branch or fork:
-RC_SCRIPT_URL="https://raw.githubusercontent.com/unofficial-unifi/unifi-pfsense/master/rc.d/unifi.sh"
+RC_SCRIPT_URL="https://raw.githubusercontent.com/jamfrade/unifi-pfsense/master/rc.d/unifi.sh"
 
 CURRENT_MONGODB_VERSION=mongodb42
 
